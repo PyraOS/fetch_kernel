7029c8af821a0ee35446183c75467f50d2a92a1f
Wally4000 <catapultit@me.com>
2024-09-02 08:56:37 +1000

    Add pyra_defconfig and use current kernel version

--- build.sh
+++ build.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-KERNEL_VERSION=6.10.7
+KERNEL_VERSION=5.6.19
+# KERNEL_VERSION=6.10.7
 
 KERNEL_MAJOR=$(echo $KERNEL_VERSION | cut -d '.' -f1)
 KERNEL_MINOR=$(echo $KERNEL_VERSION | cut -d '.' -f2)
 KERNEL_PATCH=$(echo $KERNEL_VERSION | cut -d '.' -f3)
 
-echo $KERNEL_MAJOR
-echo $KERNEL_MINOR
-echo $KERNEL_PATCH
 
 echo "Fetching specified kernel"
 wget "https://cdn.kernel.org/pub/linux/kernel/v"$KERNEL_MAJOR".x/linux-"$KERNEL_VERSION".tar.xz"
@@ -17,8 +15,9 @@ wget "https://cdn.kernel.org/pub/linux/kernel/v"$KERNEL_MAJOR".x/linux-"$KERNEL_
 echo "Extracting Kernel"
 tar -xf linux-"$KERNEL_VERSION".tar.xz
 
-echo "Copying dtsi files to kernel folder"
+echo "Copying dtsi and pyra_defconfig file to kernel folder"
 
+cp config/* linux-"$KERNEL_VERSION"/arch/arm/configs
 cp dtsi/* linux-"$KERNEL_VERSION"/arch/arm/boot/dts
 
 # Still need a kernel conf here before this is useful
