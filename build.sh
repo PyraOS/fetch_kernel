4c0c447d1fc966331eb5409670d2d890bf92a83a
Wally4000 <catapultit@me.com>
2024-09-02 10:37:20 +1000

    add Cflags

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@
 KERNEL_VERSION=5.6.19
 # KERNEL_VERSION=6.10.7
 
+# flex bison rsync libssl-dev
+
 KERNEL_MAJOR=$(echo $KERNEL_VERSION | cut -d '.' -f1)
 KERNEL_MINOR=$(echo $KERNEL_VERSION | cut -d '.' -f2)
 KERNEL_PATCH=$(echo $KERNEL_VERSION | cut -d '.' -f3)
@@ -23,7 +25,7 @@ cp dtsi/* linux-"$KERNEL_VERSION"/arch/arm/boot/dts
 cd linux-"$KERNEL_VERSION"
 # Still need a kernel conf here before this is useful
 #echo "Building as a .deb file" 
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- pyra_defconfig bindeb-pkg -j `nproc`
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- CFLAGS="-march=armv7-a -mfloat-abi=hard -mfpu=vfp" CXXFLAGS="-march=armv7-a -mfloat-abi=hard -mfpu=vfp" pyra_defconfig bindeb-pkg -j `nproc`
 
 #echo "building DTBS"
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- pyra_defconfig dtbs -j `nproc`
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- CFLAGS="-march=armv7-a -mfloat-abi=hard -mfpu=vfp" CXXFLAGS="-march=armv7-a -mfloat-abi=hard -mfpu=vfp" pyra_defconfig dtbs -j `nproc`
