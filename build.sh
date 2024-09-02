137863c44dc20c443284ca95adab9e245596a333
Wally4000 <catapultit@me.com>
2024-09-02 12:00:43 +1000

    Use CPU_FLAGS variable

--- build.sh
+++ build.sh
@@ -9,6 +9,8 @@ KERNEL_MAJOR=$(echo $KERNEL_VERSION | cut -d '.' -f1)
 KERNEL_MINOR=$(echo $KERNEL_VERSION | cut -d '.' -f2)
 KERNEL_PATCH=$(echo $KERNEL_VERSION | cut -d '.' -f3)
 
+CPU_FLAGS="-mcpu=cortex-a15 -mfpu=neon -mfloat-abi=hard -marm"
+
 if [ ! -f linux-"$KERNEL_VERSION".tar.xz ]; then
 echo "Fetching specified kernel"
 wget "https://cdn.kernel.org/pub/linux/kernel/v"$KERNEL_MAJOR".x/linux-"$KERNEL_VERSION".tar.xz"
@@ -25,7 +27,7 @@ cp dtsi/* linux-"$KERNEL_VERSION"/arch/arm/boot/dts
 cd linux-"$KERNEL_VERSION"
 # Still need a kernel conf here before this is useful
 #echo "Building as a .deb file" 
-make ARCH=arm CROSS_COMPILE=arm-none-eabi- CFLAGS="-march=armv7-a -mfloat-abi=hard -mfpu=vfp" CXXFLAGS="-march=armv7-a -mfloat-abi=hard -mfpu=vfp" pyra_defconfig bindeb-pkg -j `nproc`
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- CFLAGS="$CPU_FLAGS" pyra_defconfig bindeb-pkg -j `nproc`
 
 #echo "building DTBS"
-make ARCH=arm CROSS_COMPILE=arm-none-eabi- CFLAGS="-march=armv7-a -mfloat-abi=hard -mfpu=vfp" CXXFLAGS="-march=armv7-a -mfloat-abi=hard -mfpu=vfp" pyra_defconfig dtbs -j `nproc`
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- CFLAGS="$CPU_FLAGS" pyra_defconfig dtbs -j `nproc`
